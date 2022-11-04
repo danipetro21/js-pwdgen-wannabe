7ddde28a866e9eea318fe8a1824c64d9119d1618
danipetro21 <idany346@gmail.com>
2022-11-04 17:25:15 +0100

    add colore preferito

--- js/main.js
+++ js/main.js
@@ -2,13 +2,15 @@
 
 let nome;
 let cognome;
+let coloreP;
 let password;
 
 // input utente
 
 nome = window.prompt("Inserisci il tuo nome","Nome");
 cognome = window.prompt("Inserisci il tuo cognome","Cognome");
+coloreP = window.prompt("Inserisci il tuo colore preferito","Colore preferito");
 
 // scrittura password su h1 by ID
 
-document.getElementById("pass").innerHTML = (`${nome}${cognome}21`);
+document.getElementById("pass").innerHTML = (`${nome}${cognome}${coloreP}21`);
